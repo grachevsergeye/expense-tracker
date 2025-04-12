4adbcc8f5aec5d7989480bfc1bbb5b1b97ee9bea
grachevsergeye <sergey_grachev_03@mail.ru>
2025-04-12 23:59:06 +0300

    start

---
+++ backend/routes/authRoutes.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const { protect } = require("../middleware/authMiddleware");
+
+const {
+    registerUser,
+    loginUser,
+    getUserInfo,
+} = require("../controllers/authController");
+const upload = require("../middleware/uploadMiddleware"); //.default
+
+const router = express.Router();
+
+router.post("/register", registerUser);
+router.post("/login", loginUser);
+router.get("/getUser", protect, getUserInfo);
+
+router.post("/upload-image", upload.single("image"), (req, res) => {
+    if (!req.file) {
+        return res.status(400).json({ message: "No file uploaded" });
+    }
+    const imageUrL = `${req.protocol}://${req.get("host")}/uploads/${
+        req.file.filename
+    }`;
+    res.status(200).json({ imageUrL });
+});
+
+module.exports = router;
---
+++ frontend/expese-tracker/src/App.jsx
@@ -0,0 +1,45 @@
+import React from 'react'
+
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+} from "react-router-dom";
+import Login from "./pages/Auth/Login";
+import SignUp from "./pages/Auth/SignUp";
+import Home from "./pages/dashboard/Home";
+import Income from "./pages/Dashboard/Income";
+import Expense from "./pages/Dashboard/Expense";
+import UserProvider from "./context/UserContext";
+
+const App = () => {
+  return (
+   <UserProvider>
+    <div>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Root />}/>
+          <Route path="/login" exact element={<Login/>}/>
+          <Route path="/signUp" exact element={<SignUp/>}/>
+          <Route path="/dashboard" exact element={<Home/>}/>
+          <Route path="/expense" exact element={<Expense/>}/>
+          <Route path="/income" exact element={<Income/>}/>
+        </Routes>
+      </Router>
+    </div>
+   </UserProvider>
+  );
+};
+
+export default App;
+
+const Root = () => {
+  const isAuthenticated = !!localStorage.getItem("token");
+
+  return isAuthenticated ? (
+    <Navigate to="/dashboard"/>
+  ) : (
+    <Navigate to="/login" />
+  );
+};
